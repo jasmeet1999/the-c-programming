6c9ca4f7fdc9620898e5cfd4b2b0b99bf767f75d
Jasmeet Singh Khokhar <jasmeetsinghkhokhar@gmail.com>
2019-11-29 22:37:37 +0530

    add Exercise 1.3

---
+++ Chapter01/Exercise_1-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main()
+{
+	float farh,celsius;
+	int lower,upper,step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	farh = lower;
+
+	printf("Fahrenheit to Celsius table\n");
+	while(farh <= upper)
+	{
+		celsius = (5.0/9.0) * (farh - 32.0);
+		printf("%3.0f %6.1f\n",farh,celsius);
+		farh = farh + step;
+	}
+}
